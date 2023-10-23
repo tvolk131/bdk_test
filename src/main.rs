9e3b1c2318ab6696054ab3947cef4cc5fdc09eb1
Tommy Volk <tvolk131@gmail.com>
2023-10-22 18:59:16 -0700

    Change code to matck bdk example

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,107 @@
-extern crate bdk;
-extern crate bitcoin;
+const DB_MAGIC: &str = "bdk_wallet_electrum_example";
+const SEND_AMOUNT: u64 = 5000;
+const STOP_GAP: usize = 50;
+const BATCH_SIZE: usize = 5;
 
+use std::io::Write;
 use std::str::FromStr;
 
-use bdk::bitcoin::Network;
-use bdk::blockchain::{Blockchain, ElectrumBlockchain};
-use bdk::database::MemoryDatabase;
-use bdk::electrum_client::Client;
-use bdk::keys::bip39::Mnemonic;
-use bdk::miniscript::descriptor::Wpkh;
-use bdk::miniscript::Descriptor;
-use bdk::miniscript::psbt::PsbtExt;
-use bdk::wallet::{AddressIndex, Wallet};
-use bdk::{SyncOptions, FeeRate, SignOptions};
-use bitcoin::Address;
-use bitcoin::bip32::{ExtendedPrivKey, ExtendedPubKey};
-use bitcoin::psbt::PartiallySignedTransaction;
-use bitcoin::secp256k1::Secp256k1;
+use bdk::bitcoin::Address;
+use bdk::wallet::Update;
+use bdk::SignOptions;
+use bdk::{bitcoin::Network, Wallet};
+use bdk_electrum::{
+    electrum_client::{self, ElectrumApi},
+    ElectrumExt, ElectrumUpdate,
+};
+use bdk_file_store::Store;
 
-fn main() {
-    // 1. Generate a public key (we're hardcoding the mnemonic for simplicity, and so we can add some regtest funds to it)
-    let mnemonic: Mnemonic = Mnemonic::from_entropy(&[
-        64, 139, 40, 92, 18, 56, 54, 0, 79, 75, 136, 66, 199, 196, 131, 114, 222, 19, 130, 69, 12,
-        13, 67, 154, 243, 69, 186, 127, 196, 154, 207, 112,
-    ])
-    .unwrap();
-    let seed = mnemonic.to_seed("");
-    println!("{:?}", mnemonic.word_iter().collect::<Vec<&str>>());
-    println!("{:?}", seed);
-    let secp = Secp256k1::new();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let db_path = std::env::temp_dir().join("bdk-electrum-example");
+    let db = Store::<bdk::wallet::ChangeSet>::new_from_path(DB_MAGIC.as_bytes(), db_path)?;
+    let external_descriptor = "wpkh(tprv8ZgxMBicQKsPdy6LMhUtFHAgpocR8GC6QmwMSFpZs7h6Eziw3SpThFfczTDh5rW2krkqffa11UpX3XkeTTB2FvzZKWXqPY54Y6Rq4AQ5R8L/84'/1'/0'/0/*)";
+    let internal_descriptor = "wpkh(tprv8ZgxMBicQKsPdy6LMhUtFHAgpocR8GC6QmwMSFpZs7h6Eziw3SpThFfczTDh5rW2krkqffa11UpX3XkeTTB2FvzZKWXqPY54Y6Rq4AQ5R8L/84'/1'/0'/1/*)";
 
-    // Generate the master private key
-    let master_key = ExtendedPrivKey::new_master(Network::Testnet, &seed).unwrap();
-    let master_pub = ExtendedPubKey::from_priv(&secp, &master_key);
-
-    let descriptor = Descriptor::Wpkh(Wpkh::new(master_pub.public_key).unwrap());
-
-    // Create the wallet
-    let wallet = Wallet::new(
-        &descriptor.to_string(),
-        Some(&descriptor.to_string()),
+    let mut wallet = Wallet::new(
+        external_descriptor,
+        Some(internal_descriptor),
+        db,
         Network::Testnet,
-        MemoryDatabase::new(),
-    )
-    .unwrap();
+    )?;
 
-    let blockchain =
-        ElectrumBlockchain::from(Client::new("ssl://electrum.blockstream.info:60002").unwrap());
+    let address = wallet.get_address(bdk::wallet::AddressIndex::New);
+    println!("Generated Address: {}", address);
 
-    // Sync the wallet
-    wallet.sync(&blockchain, SyncOptions::default()).unwrap();
+    let balance = wallet.get_balance();
+    println!("Wallet balance before syncing: {} sats", balance.total());
 
-    println!("Wallet balance: {}", wallet.get_balance().unwrap());
+    print!("Syncing...");
+    let client = electrum_client::Client::new("ssl://electrum.blockstream.info:60002")?;
 
-    let sender_public_key = wallet.get_address(AddressIndex::Peek(0)).unwrap().address;
-    let receiver_public_key = Address::from_str("tb1qdwuyjaa5tcm0mrucla8vwk8fuluuat4az3dxre").unwrap().require_network(Network::Testnet).unwrap();
-    println!("Sender Pubkey: {}", sender_public_key);
-    println!("Receiver Pubkey: {:#?}", receiver_public_key);
+    let prev_tip = wallet.latest_checkpoint();
+    let keychain_spks = wallet
+        .spks_of_all_keychains()
+        .into_iter()
+        .map(|(k, k_spks)| {
+            let mut once = Some(());
+            let mut stdout = std::io::stdout();
+            let k_spks = k_spks
+                .inspect(move |(spk_i, _)| match once.take() {
+                    Some(_) => print!("\nScanning keychain [{:?}]", k),
+                    None => print!(" {:<3}", spk_i),
+                })
+                .inspect(move |_| stdout.flush().expect("must flush"));
+            (k, k_spks)
+        })
+        .collect();
 
-    // 2. Create a PSBT that pays from the public key
-    // For this example we'll pay to ourselves, but you can pay to any address
+    let (
+        ElectrumUpdate {
+            chain_update,
+            relevant_txids,
+        },
+        keychain_update,
+    ) = client.scan(prev_tip, keychain_spks, None, None, STOP_GAP, BATCH_SIZE)?;
+
+    println!();
+
+    let missing = relevant_txids.missing_full_txs(wallet.as_ref());
+    let graph_update = relevant_txids.into_confirmation_time_tx_graph(&client, None, missing)?;
+
+    let wallet_update = Update {
+        last_active_indices: keychain_update,
+        graph: graph_update,
+        chain: Some(chain_update),
+    };
+    wallet.apply_update(wallet_update)?;
+    wallet.commit()?;
+
+    let balance = wallet.get_balance();
+    println!("Wallet balance after syncing: {} sats", balance.total());
+
+    if balance.total() < SEND_AMOUNT {
+        println!(
+            "Please send at least {} sats to the receiving address",
+            SEND_AMOUNT
+        );
+        std::process::exit(0);
+    }
+
+    let faucet_address = Address::from_str("mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt")?
+        .require_network(Network::Testnet)?;
+
     let mut tx_builder = wallet.build_tx();
-    tx_builder.add_recipient(receiver_public_key.script_pubkey(), 600).fee_rate(FeeRate::default_min_relay_fee());
-    let (mut psbt, _details) = tx_builder.finish().unwrap();
-    // _details.transaction.unwrap().input.first().unwrap().witness;
-    println!("Inputs: {:#?}", psbt.inputs);
+    tx_builder
+        .add_recipient(faucet_address.script_pubkey(), SEND_AMOUNT)
+        .enable_rbf();
 
-    // 3. Sign the PSBT (Assuming hardware wallet integration)
-    // For this example, we'll assume that `sign_with_hardware_wallet` is a function you've implemented
-    // that uses a hardware wallet to sign the PSBT.
-    println!();
-    println!("Unsigned PSBT: {}", psbt.serialize_hex());
-    println!();
-    for input in &psbt.inputs {
-        println!("PSBT Input before signing: {:#?}", input);
-        println!();
-    }
-    // psbt = sign_with_hardware_wallet(psbt, &wallet);
-    // wallet.finalize_psbt(&mut psbt, SignOptions::default()).unwrap();
-    let finalized = wallet.sign(&mut psbt, SignOptions::default()).unwrap();
+    let mut psbt = tx_builder.finish()?;
+    let finalized = wallet.sign(&mut psbt, SignOptions::default())?;
     assert!(finalized);
-    println!("Signed PSBT: {}", psbt.serialize_hex());
-    println!();
-    for input in &psbt.inputs {
-        println!("PSBT Input after signing: {:#?}", input);
-        println!();
-    }
 
-    // 4. Broadcast the signed transaction to the blockchain
-    psbt.finalize_mut(&secp).expect("Failed to finalize PSBT");
-    let tx = psbt.extract(&secp).unwrap();
-    blockchain.broadcast(&tx).unwrap();
+    let tx = psbt.extract_tx();
+    client.transaction_broadcast(&tx)?;
+    println!("Tx broadcasted! Txid: {}", tx.txid());
 
-    // 5. Wait for the transaction to be confirmed
-    // This is a simplified example; you may want to implement more robust logic.
-    loop {
-        wallet.sync(&blockchain, SyncOptions::default()).unwrap();
-        if wallet
-            .get_tx(&tx.txid(), true)
-            .unwrap()
-            .unwrap()
-            .confirmation_time
-            .is_some()
-        {
-            break;
-        }
-        std::thread::sleep(std::time::Duration::from_secs(5));
-    }
+    Ok(())
 }
-
-// Dummy function to represent hardware wallet signing
-// fn sign_with_hardware_wallet(mut psbt: PartiallySignedTransaction, wallet: &Wallet<MemoryDatabase>) -> PartiallySignedTransaction {
-//     wallet.sign(&mut psbt, SignOptions::default()).unwrap();
-//     psbt
-// }
